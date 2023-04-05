36c06ac16225ea88842204b81096372981461eab
AQSASABIR1 <eqsa.saber000@gmail.com>
2023-04-05 16:36:10 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include "includes/header.php";  ?>
+<?php include "portfolio.php";  ?>
+<?php include "about.php";  ?>
+<?php include "contact.php";  ?>
+<?php include "includes/footer.php";  ?>
