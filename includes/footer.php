51006b8466d03c9c9ad34641f189f6ff8bf5ef67
Aqsa Sabir <eqsa.saber000@gmail.com>
2023-04-09 08:29:42 +0500

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -5,18 +5,15 @@
                     <!-- Footer Location-->
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <h4 class="text-uppercase mb-4">Location</h4>
-                        <p class="lead mb-0">  <?php echo get_site_setting ($con,  'b_address' ); ?></p>
+                        <p class="lead mb-0"> <?php echo get_site_setting ($con,  'b_address' ); ?></p>
                     </div>
                     <!-- Footer Social Icons-->
                     <div class="col-lg-4 mb-5 mb-lg-0">
-                    <h4 class="text-uppercase mb-4">Around the Web</h4>
-                   <?php
-                    $sql="SELECT * FROM site_settings WHERE id in(5,6,7,8)";
-                    $social_icons[]= "echo get_site_setting ($con,  'social_icons' )"; ?> 
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://<php echo $social_icons[0]; ?>"><i class="fab fa-fw fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://<php echo $social_icons[1]; ?>"><i class="fab fa-fw fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://<php echo $social_icons[2]; ?>"><i class="fab fa-fw fa-linkedin-in"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://<php echo $social_icons[3]; ?>"><i class="fab fa-fw fa-dribbble"></i></a>
+                    <h4 class="text-uppercase mb-4">Around the Web</h4>                 
+                        <a class="btn btn-outline-light btn-social mx-1" href="https:// <?php echo get_site_setting ($con,  'fb_social_link' ); ?>"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https:// <?php echo get_site_setting ($con,  'tw_social_link' ); ?>"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https:// <?php echo get_site_setting ($con,  'ln_social_link' ); ?>"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https:// <?php echo get_site_setting ($con,  'dr_social_link' ); ?>"><i class="fab fa-fw fa-dribbble"></i></a>
                     </div>
                     <!-- Footer About Text-->
                     <div class="col-lg-4">
@@ -31,39 +28,6 @@
         <!-- Copyright Section-->
         <div class="copyright py-4 text-center text-white">
             <div class="container"><small>Copyright &copy;  <?php echo get_site_setting ($con,  'copyright_text' );  $con->close(); ?></small></div>
-        </div>
-        <!-- Portfolio Modals-->
-        <!-- Portfolio Modal 1-->
-        <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" aria-labelledby="portfolioModal1" aria-hidden="true">
-            <div class="modal-dialog modal-xl">
-                <div class="modal-content">
-                    <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
-                    <div class="modal-body text-center pb-5">
-                        <div class="container">
-                            <div class="row justify-content-center">
-                                <div class="col-lg-8">
-                                    <!-- Portfolio Modal - Title-->
-                                    <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0"><?php echo $row["name"]; ?></h2>
-                                    <!-- Icon Divider-->
-                                    <div class="divider-custom">
-                                        <div class="divider-custom-line"></div>
-                                        <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                                        <div class="divider-custom-line"></div>
-                                    </div>
-                                    <!-- Portfolio Modal - Image-->
-                                    <img class="img-fluid rounded mb-5" src="<?php echo $row["img_url"]; ?>" alt="..." />
-                                    <!-- Portfolio Modal - Text-->
-                                    <p class="mb-4"><?php echo $row["description"]; ?></p>
-                                    <button class="btn btn-primary" data-bs-dismiss="modal">
-                                        <i class="fas fa-xmark fa-fw"></i>
-                                        Close Window
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
         <!-- 
         <!-- Bootstrap core JS-->
